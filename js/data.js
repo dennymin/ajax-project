62fc6d13fab9c4fbff517a102f0103e66281a1b7
Denny Min <denden223@gmail.com>
2021-07-27 15:13:42 -0700

    Added images to images folder, set up background and modal for asking the first question of where the user wants to check information for. Added functionality to the submit button as well so that it saves an entry in the data model. #1

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
 /* exported data */
+var data = {
+  locations: [],
+  view: 'entry-form',
+  nextEntryId: 1,
+  primary: null
+};
+
+var previousDataJSON = localStorage.getItem('weather-data');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function storeLocal(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('weather-data', dataJSON);
+}
+window.addEventListener('beforeunload', storeLocal);
