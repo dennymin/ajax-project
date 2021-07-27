dfa2a2491fb9f59b18a5b2c8242ddf4b0acaecdc
Denny Min <denden223@gmail.com>
2021-07-27 15:36:40 -0700

    Changed submission value from undefined to ''. #1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var $searchBar = document.querySelector('.search-bar');
 var $searchSubmitButton = document.querySelector('.submit-search');
 function saveLocationToData(event) {
-  if ($searchBar.value !== undefined) {
+  if ($searchBar.value !== '') {
     data.locations.push({ location: $searchBar.value, entryId: data.nextEntryId });
     data.nextEntryId++;
   }
