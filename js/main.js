96b88b9b6a77d02ec55ddac6013077cb4ad6014b
Denny Min <denden223@gmail.com>
2021-07-28 10:29:38 -0700

    Cleaned up data so that instead of pushing a new object after the initial question is asked, an object is pushed after the choices are filled out. #2

--- js/main.js
+++ js/main.js
@@ -1,46 +1,62 @@
 var $searchBar = document.querySelector('.search-bar');
 var $searchSubmitButton = document.querySelector('.submit-search');
-function saveLocationToData(event) {
+function queryLocation(event) {
   if ($searchBar.value !== '') {
     event.preventDefault();
-    data.locations.push({
-      Location: $searchBar.value,
-      Main: false,
-      Temperature: false,
-      High: false,
-      Low: false,
-      Wind: false,
-      Humidity: false,
-      SunsetSunrise: false
-    });
     data.editing = $searchBar.value;
     $locationAsker.classList.toggle('hidden');
     $weatherInformationChoices.classList.toggle('hidden');
-    $weatherChoicesQuestion.textContent = 'Weather Information for:';
-    var $weatherChoicesLocation = document.createElement('h1');
-    $weatherChoicesLocation.textContent = data.editing;
-    $weatherChoicesLocation.className = 'italics set-margins font-size-1rem';
-    $weatherChoicesQuestion.appendChild($weatherChoicesLocation);
+    locationWeatherInformation.location = data.editing;
+    createWeatherQuestion();
   }
 }
-$searchSubmitButton.addEventListener('click', saveLocationToData);
+$searchSubmitButton.addEventListener('click', queryLocation);
+
+function createWeatherQuestion() {
+  $weatherChoicesQuestion.textContent = 'Weather Information for:';
+  var $weatherChoicesLocation = document.createElement('h1');
+  $weatherChoicesLocation.textContent = data.editing;
+  $weatherChoicesLocation.className = 'italics set-margins font-size-1rem';
+  $weatherChoicesQuestion.appendChild($weatherChoicesLocation);
+}
 
 var $weatherInformationChoices = document.querySelector('.weather-information-choices');
 var $weatherChoicesQuestion = document.querySelector('.weather-information-choices-question');
 var $locationAsker = document.querySelector('.location-asker');
 var $weatherChoicesList = document.querySelector('.list-of-weather-choices');
 var weatherOptions = ['Main', 'Temperature', 'High', 'Low', 'Wind', 'Humidity', 'SunsetSunrise'];
+var locationWeatherInformation = {};
+for (var optionIndex = 0; optionIndex < weatherOptions.length; optionIndex++) {
+  locationWeatherInformation[weatherOptions[optionIndex]] = false;
+}
+
 function alternateIcon(event) {
-  var optionChosen = event.target.value;
   var circleOrCheck = event.target.children[0].className;
   if (event.target && event.target.nodeName === 'LI') {
     if (circleOrCheck === 'far fa-circle') {
       event.target.children[0].className = 'far fa-check-circle';
-      data.locations[0][weatherOptions[optionChosen]] = true;
+      locationWeatherInformation[weatherOptions[event.target.value]] = true;
+      // console.log(locationWeatherInformation[weatherOptions[event.target.value]]);
     } else if (circleOrCheck === 'far fa-check-circle') {
       event.target.children[0].className = 'far fa-circle';
-      data.locations[0][weatherOptions[optionChosen]] = false;
+      locationWeatherInformation[weatherOptions[event.target.value]] = false;
+      // console.log(locationWeatherInformation[weatherOptions[event.target.value]]);
     }
   }
 }
 $weatherChoicesList.addEventListener('click', alternateIcon);
+
+function appendLocationsList(event) {
+  data.locations.push(locationWeatherInformation);
+  resetOptions();
+}
+
+function resetOptions() {
+  locationWeatherInformation = {};
+  for (var optionIndex = 0; optionIndex < weatherOptions.length; optionIndex++) {
+    locationWeatherInformation[weatherOptions[optionIndex]] = false;
+  }
+}
+
+var $weatherOptionsSubmitButton = document.querySelector('.submit-choices');
+$weatherOptionsSubmitButton.addEventListener('click', appendLocationsList);
